d6c914fe36744bf1f2ba39fe557018df3940ed2a
ruvifndo <148860628+ruvifndo@users.noreply.github.com>
2023-10-24 14:23:57 +0000

    adding a python file that can take user input
    We have created a new python file with code that is able to take input from the user and then store the value on it.

--- hello-world.py
+++ hello-world.py
@@ -2,4 +2,8 @@ print("My name is Ruvina Fernando")
 
 #something more interesting
 
-print("I am 26 years old")
+# print("I am 26 years old")
+
+myName = input("What is your name?")
+
+print("Hello: " + myName)
